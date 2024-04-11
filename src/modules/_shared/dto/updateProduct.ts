aae9b9c5881f82223342760609f8fb6253a87da3
Szymon Antczak <szymon0antczak@gmail.com>
2024-04-11 12:03:59 +0000

    Fix updateProductDTO

--- src/modules/_shared/dto/updateProduct.ts
+++ src/modules/_shared/dto/updateProduct.ts
@@ -6,5 +6,5 @@ import { type DTO } from '../types';
 export const updateProductDTO = t.Object({
   name: t.String(nameValidators),
   quantity: t.Optional(t.String()),
-  unit: t.String(),
+  unit: t.Optional(t.String()),
 }) satisfies DTO;
